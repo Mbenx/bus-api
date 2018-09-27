3d8e81eccc4b601099a3642fc45e2f1200fe0a2c
Mbenx <herlambang.srihartono@gmail.com>
2018-09-27 11:27:40 +0700

    connection on Atlas mongodb still have an issue on cloud conection

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 //=== settingan
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-var router     = express.Router();
-var port       = process.env.PORT || 3000;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const router = express.Router();
+// const port = process.env.PORT || 3000;
+
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://busApi:Pass#busApi@mongoatlas-shard-00-00-zwsmf.mongodb.net:27017,mongoatlas-shard-00-01-zwsmf.mongodb.net:27017,mongoatlas-shard-00-02-zwsmf.mongodb.net:27017/test?ssl=true&replicaSet=mongoAtlas-shard-0&authSource=admin&retryWrites=true";
+MongoClient.connect(uri, function(err, db) {
+   db.close();
+});
+
 
 //==== konfigrasi bodyParser
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+// app.use(bodyParser.json());
 
-//midlleware
-router.use(function(req, res, next) {
-  console.log('Middleware berjalan pada: ', Date.now());
-  next();
-});
+// //midlleware
+// router.use(function (req, res, next) {
+//   console.log('Middleware berjalan pada: ', Date.now());
+//   next();
+// });
 
-//test router
-router.get('/', function(req, res){
-  res.json({ message: "anda di home!"});
-});
+// //test router
+// router.get('/', function (req, res) {
+//   res.json({
+//     message: "anda di home!"
+//   });
+// });
 
-router.get('/welcome', function(req, res){
-  res.json({ message: "Welcome"});
-});
+// router.get('/welcome', function (req, res) {
+//   res.json({
+//     message: "Welcome"
+//   });
+// });
 
 
 
-//===== prefix api
-app.use('/api', router);
+// //===== prefix api
+// app.use('/api', router);
 
-app.listen(port);
-console.log(' port run on.. '  + port);
+// app.listen(port);
+// console.log(' port run on.. ' + port);
